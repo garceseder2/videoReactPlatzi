e1ffe7aa2b7c8135af521c673fbd7a2f87a1d60a
Eder Garcés <garceseder2@gmail.com>
2021-01-07 16:48:09 -0500

    Creacion componentes Footer

---
+++ Platzi-Video/src/containers/NotFount.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/styles/components/NotFount.scss';
+
+const NotFount = () => (
+  <section className='no-fount'>
+    <h1 className='animated no-fount__error hinge'>404</h1>
+    <p>Pagina no encontrada :/</p>
+  </section>
+);
+
+export default NotFount;
