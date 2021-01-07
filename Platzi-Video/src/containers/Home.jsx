11c302dcd7a07e6c13c76fa8f60b1ce857c5cce8
Eder Garcés <garceseder2@gmail.com>
2021-01-07 14:43:40 -0500

    CREACION DE LA PRIMERA RUTA

--- Platzi-Video/src/containers/Home.jsx
+++ Platzi-Video/src/containers/Home.jsx
@@ -11,7 +11,7 @@ import Footer from '../components/Footer';
 import useInitialState from '../hooks/useinitialState';
 
 const API = 'http://localhost:3000/initalState';
-const App = () => {
+const Home = () => {
   const initalState = useInitialState(API);
   return (
     <div className='App'>
@@ -41,4 +41,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Home;
